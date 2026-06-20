c5e7d5aa946ff7f1b6ee39f76cd148e691fd93ae
Alonso Maurel <alonso.maurel.m@mail.pucv.cl>
2026-06-20 15:35:45 -0400

    Ajustes varios

--- run_loadtest.bat
+++ run_loadtest.bat
@@ -9,8 +9,15 @@ echo.
 echo IMPORTANTE: Los 3 nodos deben estar corriendo
 echo             antes de ejecutar esta prueba.
 echo.
-echo Conectando a localhost en puertos 5001, 5002, 5003...
+echo Conectando a nodos distribuidos (2 locales, 1 remoto)...
 echo.
-java -cp "bin;lib\mysql-connector-j.jar" loadtest.GeneradorCarga localhost 5001,5002,5003 50 60
+
+:: Ejecucion Tailscale (2 locales en tu PC, 1 remoto en PC B):
+java -cp "bin;lib\mysql-connector-j.jar" loadtest.GeneradorCarga localhost localhost:5001,localhost:5002,100.110.116.123:5003 50 60
+
+:: Ejecucion local 100% anterior:
+:: java -cp "bin;lib\mysql-connector-j.jar" loadtest.GeneradorCarga localhost 5001,5002,5003 50 60
+
 pause
+
 
